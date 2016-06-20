f49f6b8d524fa0c3707c2e42cf60df0d88739551
Fydui <gtx980sli@yahoo.com>
2016-06-20 21:46:53 +0800

    丢个辣鸡开开荒...

---
+++ 0_1_1/0_1_1.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-06-19T21:08:56
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = 0_1_1
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ 0_1_1/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void c_in(string text,int s)
+{
+    for(int i =0;i < s;i++)
+    {
+        cout << text << endl;
+    }
+    cout << "done." << endl;
+}
+
+int main()
+{
+    int userS = 0;
+    int userK = 0;
+    cout << "你想说多少句话?0v0" << endl;
+    cin >>  userS;
+    cout << "多少次呢?" << endl;
+    cin >> userK;
+    for(int l = 0; l <userS;l++)
+    {
+        string userI = "";
+        cout << "好吧,你想说啥: " << endl;
+        cin >> userI;
+        c_in(userI,userK);
+    }
+    return 0;
+}
