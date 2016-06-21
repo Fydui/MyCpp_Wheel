926cc79cc8cdc3c052331aaff9be2394242442f3
Fydui <gtx980sli@yahoo.com>
2016-06-21 22:57:30 +0800

    刷存在感

---
+++ 0_1_2/0_1_2.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-06-20T21:48:55
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = 0_1_2
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ 0_1_2/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+float C_F(float C)
+{
+    float c_f =  1.8 * C + 32.00;
+    return c_f;
+}
+
+float F_C(float F)
+{
+    float f_c =  (F - 32) / 1.8;
+    return f_c;
+}
+
+int main()
+{
+    string L;
+    float C;
+    cout << "C->F enter C, F->C enter F , Q == Quit" << endl ;
+    cin >> L;
+
+    if(L == "C")
+        {
+        cout << "enter C" << endl;
+        cin >> C;
+        cout<< "F = " << C_F(C) << endl;
+    }
+
+    else if(L == "Q")
+    {
+        cout << "Quit" << endl;
+        return 0;
+    }
+
+    else if(L == "F")
+    {
+        cout << "enter F" << endl;
+        cin >> C;
+        cout << "C = " << F_C(C) << endl;
+    }
+
+    else
+        {
+            cout << "Error!" << endl;
+    }
+
+}
