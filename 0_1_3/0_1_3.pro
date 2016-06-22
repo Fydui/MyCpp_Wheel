87ca0f33b99ad9af39641585339001ab4c62306f
Fydui <gtx980sli@yahoo.com>
2016-06-22 22:44:22 +0800

    日常打卡 经纬度转换

---
+++ 0_1_3/0_1_3.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-06-22T20:26:59
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = 0_1_3
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ 0_1_3/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+void dms(int d, int m, int s)
+{
+    const double K = 60.00;
+    double result = (m/K)+((s/K)/K)+d;
+    cout.setf(ios_base::fixed,ios_base::floatfield);
+    cout << d << " degrees, " << m << " minutes,"
+         << s << " seconds = " << result << " derees. " << endl;
+}
+
+int main()
+{
+    int d,m,s;
+    cout << "enter a latitude in dergees,mintues, and seconds: " << endl;
+    cout << "First enter the degrees:";
+    cin >> d;
+    cout << "enter the minutes of arc:";
+    cin >> m;
+    cout << "enter the senconds of arc:";
+    cin >> s;
+    dms(d,m,s);
+
+    return 0;
+}
