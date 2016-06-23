7b2acfa8f1eff3f15bf5bdf7687d181c81042cc7
Fydui <gtx980sli@yahoo.com>
2016-06-23 23:04:24 +0800

    结构的小练习

---
+++ 0_1_4/0_1_4.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-06-23T20:29:52
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = 0_1_4
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ 0_1_4/main.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+using namespace std;
+
+struct pizza
+{
+    pizza(){name = new char[50];}
+    char* name;
+    double d;
+    double kg;
+
+    ~pizza()
+    {
+        delete [] name;
+    }
+};
+
+int main()
+{
+    //pizza* mypiz = new pizza;
+    pizza mypiz;
+
+    cout << "enter pizza name" << endl;
+    cin >> mypiz.name;
+
+    cout << "enter pizza diameter" << endl;
+    cin >> mypiz.d;
+
+    cout << "enter pizza mass" << endl;
+    cin >> mypiz.kg;
+
+    cout << "pizza name: " << mypiz.name
+         << "\npizza diameter: " << mypiz.d
+         << "\npizza mass: " << mypiz.kg << endl;
+
+    return 0;
+
+}
+
