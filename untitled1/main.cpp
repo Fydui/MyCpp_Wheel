d53347d17cc0cc08cb68c63cc38d9fb0231f15d3
Fydui <gtx980sli@yahoo.com>
2016-07-02 23:45:53 +0800

    QT数据库操作 字符串转换有点问题

---
+++ untitled1/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <QtSql>
+#include <QString>
+#include <string>
+#include <QSqlQuery>
+#include <qsqlquery.h>
+#include <QDebug>
+#include <QTextCodec>
+#include <QObject>
+using namespace std;
+
+int main()
+{   //QTextCodec* codec = QTextCodec::codecForName("UTF-8");
+
+    QStringList drivers = QSqlDatabase::drivers();
+    drivers.removeAll("QSQLITE");
+    foreach (QString driver, drivers)
+    {
+        qDebug()<<"\t"<<driver;
+    }
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("LAI.db");
+    if(!db.open())
+    {
+        qDebug() <<"can't open sql";
+    }
+    QSqlQuery query;
+    query.exec(QObject::tr("CREATE TABLE LAI(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+               "ASK TEXT, ANSWER TEXT)"););
+    query.exec(QObject::tr("INSERT INTO LAI VALUES(1,'你好','你好啊')");
+    query.exec("INSERT INTO LAI VALUES(2,'你叫啥','凉果冻')");
+    query.exec("INSERT INTO LAI VALUES(3,'你多大','不告诉你')");
+    query.exec("SELECT * FROM LAI ORDER BY ID ASC");
+    while(query.next())
+    {
+        int id = query.value(0).toInt();
+        QString ask = query.value(1).toString();
+        QString answer = query.value(2).toString();
+        qDebug()<<id<<ask<<answer;
+
+    }
+
+    db.close();
+}
---
+++ untitled1/untitled1.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-06-26T22:09:53
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+QT += sql
+
+TARGET = untitled1
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
