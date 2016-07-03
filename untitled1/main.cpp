e2c252212d86bceb675fed2bb83aa3e1b2602ec7
Fydui <gtx980sli@yahoo.com>
2016-07-03 17:54:54 +0800

    QT数据库

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -10,35 +10,67 @@
 using namespace std;
 
 int main()
-{   //QTextCodec* codec = QTextCodec::codecForName("UTF-8");
-
-    QStringList drivers = QSqlDatabase::drivers();
-    drivers.removeAll("QSQLITE");
-    foreach (QString driver, drivers)
-    {
-        qDebug()<<"\t"<<driver;
-    }
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE"); //添加数据库驱动链接sqlite
     db.setDatabaseName("LAI.db");
     if(!db.open())
     {
         qDebug() <<"can't open sql";
     }
     QSqlQuery query;
-    query.exec(QObject::tr("CREATE TABLE LAI(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
-               "ASK TEXT, ANSWER TEXT)"););
-    query.exec(QObject::tr("INSERT INTO LAI VALUES(1,'你好','你好啊')");
-    query.exec("INSERT INTO LAI VALUES(2,'你叫啥','凉果冻')");
-    query.exec("INSERT INTO LAI VALUES(3,'你多大','不告诉你')");
-    query.exec("SELECT * FROM LAI ORDER BY ID ASC");
+    query.exec(QObject::tr("CREATE TABLE LAI(ID INTEGER PRIMARY KEY AUTOINCREMENT,ASK TEXT, ANSWER TEXT)"));
+    query.exec(QObject::tr("INSERT INTO LAI VALUES(1,'你好','你好啊')"));
+    query.exec(QObject::tr("INSERT INTO LAI VALUES(2,'你叫啥','凉果冻')"));
+    query.exec(QObject::tr("INSERT INTO LAI VALUES(3,'你多大','不告诉你')"));
+    query.exec(QObject::tr("SELECT * FROM LAI ORDER BY ID ASC"));
+    QTextStream cout(stdout,QIODevice::WriteOnly); //中文输出
     while(query.next())
     {
         int id = query.value(0).toInt();
         QString ask = query.value(1).toString();
         QString answer = query.value(2).toString();
-        qDebug()<<id<<ask<<answer;
+        //qDebug()<<id<<ask<<answer;
+        //string sask = ask.toStdString();
+        //string sanswer = answer.toStdString();
+        cout << " ID: " <<id << endl;
+        cout << " ASK: "<<ask << endl;
+        cout <<" ANSWER: " << answer << endl;
 
     }
+    query.exec(QObject::tr("SELECT * FROM LAI WHERE ASK LIKE '你好%';"));
+    while(query.next())
+    {
+       QString tes = query.value(2).toString();
+        cout <<"LIKE: " <<tes;
+    }
+
 
     db.close();
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- untitled1/untitled1.pro
+++ untitled1/untitled1.pro
@@ -17,3 +17,5 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp
+
+
