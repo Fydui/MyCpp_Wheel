9eb6e749da1dd23dfd21c567963dcbe4d243f2d7
Fydui <gtx980sli@yahoo.com>
2016-11-26 23:18:08 +0800

    no message

--- StringBad/StringBad.cpp
+++ StringBad/StringBad.cpp
@@ -4,9 +4,9 @@ int StringBad::num_strings = 0;
 
 StringBad::StringBad()
 {
-    len = 4;
-    str = new char[len+1];
-    strcpy(str,"C++");
+    len = 0;
+    str = new char[1];
+    str[0] = '\0';
     num_strings++;
     cout << "目前有: "<<num_strings << "个对象" << endl;
 }
@@ -28,6 +28,13 @@ StringBad::StringBad(char* s)
     cout << "第 "<<num_strings<<" 个对象:"<< str <<endl;
 }
 
+StringBad::StringBad(const char* s){
+    len = strlen(s);
+    str = new char[len+1];
+    strcpy(str,s);
+    num_strings++;
+}
+
 StringBad::StringBad(const StringBad &st){
     num_strings++;
     int len = st.len;
@@ -35,16 +42,48 @@ StringBad::StringBad(const StringBad &st){
     strcpy(str,st.str);
     cout <<"复制构造函数被调用.值:" << str <<endl;
 }
-StringBad::operator =(const StringBad &st){
+StringBad & StringBad::operator =(const StringBad &st){
     if(this == &st) return *this;
-    delete str;
+    delete [] str;
     len = st.len;
     str = new char[len+1];
     strcpy(str,st.str);
+    return *this;
+}
+
+StringBad & StringBad::operator =(const char* st){
+    delete [] str;
+    len = strlen(st);
+    str = new char[len+1];
+    strcpy(str,st);
     return *this;
 }
 ostream & operator<<(ostream &os, StringBad &st)
 {
     os << st.str;
     return os;
+}
+
+bool operator<(const StringBad &st,const StringBad &st2){
+    if(strcmp(st.str,st2.str) < 0) return true;
+    else return false;
+}
+
+bool operator>(const StringBad &st,const StringBad &st2){
+    return st2 < st;
+}
+
+bool operator==(const StringBad &st,const StringBad &st2){
+    if(strcmp(st.str,st2.str) == 0) return true;
+    else return false;
+}
+istream & operator>>(istream &is, StringBad &st){
+    is >> st.str;
+    return is;
+}
+char StringBad::operator [](int i){
+    return str[i];
+}
+const char StringBad::operator [](int i) const{
+    return str[i];
 }
--- StringBad/StringBad.h
+++ StringBad/StringBad.h
@@ -9,9 +9,21 @@ public:
     StringBad();
     ~StringBad();
     StringBad(char* s);
+    StringBad(const char* s);
     StringBad(const StringBad &st);      //复制构造函数
     StringBad & operator=(const StringBad &st);//赋值运算符
+    StringBad & operator=(const char *st);
+
     friend std::ostream & operator<<(std::ostream &os, StringBad &st);
+    friend std::istream & operator >>(std::istream &is,StringBad &st);
+    friend bool operator<(const StringBad &st,const StringBad &st2);
+    friend bool operator>(const StringBad &st,const StringBad &st2);
+    friend bool operator==(const StringBad &st,const StringBad &st2);
+
+    char & operator[](int i);
+    const char & operator [](int i) const;
+
+    static int HowMany(){return num_strings;}
 
 private:
     char* str;
