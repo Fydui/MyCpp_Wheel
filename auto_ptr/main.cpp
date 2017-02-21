93118ba183828317a53825bfaacaf077c1239bb1
Fydui <gtx980sli@yahoo.com>
2017-02-21 22:49:39 +0800

    智能指针

---
+++ auto_ptr/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+class Report
+{
+private:
+    string str;
+public:
+    Report(const string s) : str(s)
+    {cout << "Objectt created:" << endl;}
+    ~Report() {cout << "Object deleted" << endl;}
+    void comment() const {cout << str << endl;}
+};
+int main()
+{
+    {
+        auto_ptr<Report> ps(new Report("auto_ptr"));
+        ps->comment();
+    }
+    {
+        shared_ptr<Report> ps(new Report("shared_ptr"));
+        ps->comment();
+    }
+    {
+        unique_ptr<Report> ps(new Report("unique_ptr"));
+        ps->comment();
+    }
+    return 0;
+}
