5d72e550c434e1842c03e160165b038c9f905208
Fydui <gtx980sli@yahoo.com>
2017-03-09 23:20:08 +0800

    字符串分割变换机

---
+++ EasyRPD/mainwindow.cpp
@@ -0,0 +1,24 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QString>
+using namespace std;
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString id = ui->textEdit->toPlainText();
+    if(id == "") return;
+    //算法加密
+    if(pd != "") ui->textBrowser_2->clear();
+    ui->textBrowser_2->append(pd);
+}
