837dd79df34a524fbcd9df898345baff01425cc6
Fydui <gtx980sli@yahoo.com>
2017-04-28 23:22:35 +0800

    no message

---
+++ manip/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter an integer;";
+    int n;
+    cin >> n;
+
+    cout << "n    n*n" <<endl;
+    cout << n << "    " << n*n << "(decimal)" << endl;
+    cout << hex;
+    cout << n << "    ";
+    cout << n*n << " (hexadecimal)" << endl;
+    cout << oct << n << "    " << n * n << "(octal)" << endl;
+    dec(cout);
+    cout << n << "    "<< n *n << " (decimal)" << endl;
+
+    return 0;
+}
---
+++ write/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    const char * s1 = "Florida";
+    const char * s2 = "Kansas";
+    const char * s3 = "Euphoria";
+
+    int len = strlen(s2);
+    cout << "Increasing LOOP" << endl;
+
+    int i;
+    for(i = 1; i <= len; i++)
+    {
+        cout.write(s2,i);
+        cout <<endl;
+    }
+    cout << "Decreasing LOOP:";
+    for(i = len; i > 0; i--)
+    cout.write(s2,i) << endl;
+    cout << "Exceeding string length: " << endl;
+    cout.write(s2,len+5) << endl;
+
+    return 0;
+}
