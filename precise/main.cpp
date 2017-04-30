93ebe8c98d845c9a92c6935c076a2fbc034678bf
Fydui <gtx980sli@yahoo.com>
2017-04-30 23:17:52 +0800

    no message

---
+++ precise/main.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    float p1 = 20.40;
+    float p2 = 1.9 + 8.0 /9.0;
+
+    cout << "p1:" << p1 << endl;
+    cout << "p2:" << p2 << endl;
+
+    cout.precision(2);
+    cout << "p1(2): " << p1 << endl;
+    cout << "p2(2):" << p2 << endl;
+
+    return 0;
+}
---
+++ showpt/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float p1 = 12.40;
+    float p2 = 1.9+8.0/9.0;
+
+    cout.setf(ios_base::showpoint);
+    cout << "p1:" << p1 << endl;
+    cout << "p2:" << p2 << endl;
+
+    cout.precision(2);
+    cout << "(2)p1:" << p1 << endl;
+    cout << "(2)p2:" << p2 << endl;
+
+    return 0;
+}
+
